0238476b3726d57f7bc2accea874e60e1705f597
Deeptanshu <deeptanshu.@Mac.lan>
2025-04-06 20:50:42 -0400

    Added Deployment for Heroku

--- app.py
+++ app.py
@@ -32,6 +32,3 @@ def predict_post(request: PredictRequest):
         return {"prediction": float(pred[0])}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
